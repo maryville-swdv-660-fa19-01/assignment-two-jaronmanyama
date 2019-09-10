814c47f9ea098a95d3aa2bc50a1dda681e78aecd
jaronmanyama <42523191+jaronmanyama@users.noreply.github.com>
2019-09-09 19:05:57 -0700

    corrected so that method increments down by 1 and not up by 1

--- spaceman/tests/test_game_model.py
+++ spaceman/tests/test_game_model.py
@@ -48,11 +48,11 @@ class GameModelTests( TestCase ):
             guessed_word_state= ['','','S','','W','O','R',''],
             letters_guessed = ['S', 'A', 'W', 'O', 'R','C'],
             guesses_allowed= 5, 
-            guesses_taken= expectedGuessesTaken
+            guesses_taken= expectedGuessesTaken - 1
         )
 
         game.handleGuess('X')
-        self.assertEquals( expectedGuessesTaken, game.guesses_taken )
+        self.assertEqual( expectedGuessesTaken, game.guesses_taken )
     
 
     ### guessed_word_state field
